ffebb0457a3c48733effd7b3f9401b9080efe398
Kort <r.benard87@gmail.com>
2018-10-15 21:53:19 +0200

    Mise en place projet Domain

---
+++ Domain/Base/DomainObject.cs
@@ -0,0 +1,28 @@
+﻿using Core.Domains.Base;
+
+namespace Domain.Base
+{
+    /// <summary>
+    /// Classe parente de tout objet de domaine
+    /// </summary>
+    public class DomainObject : IDomainObject
+    {
+        /// <summary>
+        /// Permet de cloner un objet.
+        /// </summary>
+        /// <returns>L'objet retourné est à caster pour recuperer son type.</returns>
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        /// <summary>
+        /// Définition du caractère vide d'un objet.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsEmpty()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Domain/Readme.md
@@ -0,0 +1,3 @@
+﻿# Projet Domain
+
+Ce projet contient la définition des Dtos (Data Transfer Object) de l'application.
