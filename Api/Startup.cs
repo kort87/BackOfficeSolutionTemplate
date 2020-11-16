bbf3e94ebf74d031fbaf4f37c70c1879edb00f3b
David Goodenough <r.benard87@gmail.com>
2020-11-16 21:53:44 +0100

    Fix swagger issues after package upgrade Fix project configuration after upgrade to version 3.1

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,19 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Core.Data.Base;
+﻿using Core.Data.Base;
 using Data;
-using Data.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
 
 namespace Api
 {
@@ -29,16 +20,16 @@ namespace Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info
+                c.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Version = "v1",
                     Title = "TemplateBackEnd Api",
                     Description = "Bibliothèque Api du projet back-end template",
-                    TermsOfService = "None",
-                    Contact = new Contact() { Name = "Template BackEnd Api", Email = "r.benard87@gmail.com" }
+                    TermsOfService = null,
+                    Contact = new OpenApiContact() { Name = "Template BackEnd Api", Email = "youremail@gmail.com" }
                 });
             });
 
@@ -49,9 +40,9 @@ namespace Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.EnvironmentName.ToUpper().Equals("DEVELOPMENT"))
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -61,14 +52,15 @@ namespace Api
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseStaticFiles();
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Template BackEnd V1");
             });
 
-            
+            app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
 
         /// <summary>
